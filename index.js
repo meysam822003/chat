16a4007a357a87eb1c36c0603dd5d756fad6eb32
meysamkh82 <khalilimeysam82@yahoo.com>
2024-01-31 00:32:59 +0330

    hello in index.js by console.log

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let tabligh = document.querySelector('.tabligh');
 let btnTabligh = document.querySelector('.btn-tabligh');
+console.log('hello');
 setTimeout(()=>{
     tabligh.style.bottom = 0;
 },10000)
